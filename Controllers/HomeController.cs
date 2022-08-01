45825107500d5d1c01cce4b08437db8ac7765fdc
HanzAkor <95639001+HanzAkor@users.noreply.github.com>
2022-08-01 15:15:11 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Acebook.Models;
+
+namespace Acebook.Controllers;
+
+// inheriting from Controller
+public class HomeController : Controller
+{
+    // attribute field
+    private readonly ILogger<HomeController> _logger;
+
+    //Constructor
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    // Get route
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // Get route
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Terms_and_Conditions()
+    {
+        return View();
+    }
+
+    // Error route
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
